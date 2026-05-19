f768364d7f95d293e6502830af585ffab0692c9f
novakvova <novakvova@gmail.com>
2026-05-19 18:51:02 +0300

    Working network begin

---
+++ Lesson 1. Introduction to networks/SimpleSocket/Server/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Установка кодування
+using System.Net;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Запуск сервера і програми");
+
+var hostName = Dns.GetHostName();
+Console.WriteLine($"Мій хост: {hostName}");
+
+//Ми можемо визначити адреси нашого ПК
+var localhost = await Dns.GetHostEntryAsync(hostName);
+int i = 0;
+Console.WriteLine("Оберіть IP адрес на якому " +
+    "буде працювати сервер:");
+foreach(var item in localhost.AddressList)
+{
+    Console.WriteLine($"{++i}.{item}");
+}
+
+
