19b9de1621c98abef6a9d5be4630c46c777c2859
novakvova <novakvova@gmail.com>
2026-05-19 19:57:06 +0300

    Add client

---
+++ Lesson 1. Introduction to networks/ClientSocket/MyClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Вкажіть IP сервера:");
+Console.Write("->_");
+var ip = IPAddress.Parse(Console.ReadLine());
+Console.WriteLine("Вкажіть PORT сервера:");
+Console.Write("->_");
+var port = int.Parse(Console.ReadLine());
+try
+{
+    var endPoint = new IPEndPoint(ip, port);
+    Socket clientConnect = new Socket(AddressFamily.InterNetwork,
+        SocketType.Stream, ProtocolType.Tcp);
+    clientConnect.Connect(endPoint);
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Щось пішло не так: "+ ex.Message);
+}
+
--- Lesson 1. Introduction to networks/SimpleSocket/Server/Program.cs
+++ Lesson 1. Introduction to networks/SimpleSocket/Server/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 //Установка кодування
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 Console.InputEncoding = Encoding.UTF8;
@@ -19,5 +20,44 @@ foreach(var item in localhost.AddressList)
 {
     Console.WriteLine($"{++i}.{item}");
 }
+Console.Write("->_");
+int numberIP = int.Parse(Console.ReadLine());
 
+var serverIp = localhost.AddressList[numberIP - 1];
+///Кінцева точка - MS SQL Server
+///На якому працює MS SQL Server - 1433
+///PostgreSQL - 5432
+/// У Вас є Ip адреса + порт на якому запущена програма
+int serverPort = 1087; // це порт, який буде для нашої програми
 
+Console.Title = serverIp + ":" + serverPort;
+
+//Сокет - Це спеціальний інструмент для відпрвки
+//або отримання повідомлень у вигляді байт
+//1- Створюємо кінцеву точнку, до якої може звертатися
+IPEndPoint iPEndPoint = new IPEndPoint(serverIp, serverPort);
+//2 - створюємо сокет, який може не себе приймати повідомлення
+Socket server = new Socket(AddressFamily.InterNetwork,
+    SocketType.Stream, ProtocolType.Tcp);
+try
+{
+    //Робимо прив'язку до нашої кінцевої точки
+    server.Bind(iPEndPoint);
+    //Розмір черги на отримання запитів від клієнтів
+    server.Listen(10);
+    while(true)
+    {
+        Console.WriteLine("-----Сервер очієкує клієнтів----");
+        //У цьому коді він очікує інформації від клієнтів
+        Socket client = server.Accept(); //Тут зупинка, поки хось не постуєкає
+        Console.WriteLine($"До нас постукав {client.RemoteEndPoint}");
+        client.Shutdown(SocketShutdown.Both);
+        client.Close();
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Сталася халепа Хюстон - " + ex.ToString());
+}
+
+Console.ReadKey(); // Пауза
